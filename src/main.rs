d7320908d606aecf67b13a3f38d05f71f2070bf2
Peter <sceptix@gmail.com>
2022-04-02 17:09:10 -0400

    sort out reasonable vector allocation...with_capacity!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 
+use png::{Info, OutputInfo};
+
 fn main() {
     // The decoder is a build for reader and can be used to set various decoding options
     // via `Transformations`. The default output transformation is `Transformations::IDENTITY`.
@@ -9,30 +11,39 @@ fn main() {
     let mut buf = vec![0; reader.output_buffer_size()];
     // Read the next frame. An APNG might contain multiple frames.
     let info = reader.next_frame(&mut buf).unwrap();
-    
-    println!("width {} * height {}", info.width, info.height);
-    println!("bit depth {:?}, line size {}", info.bit_depth, info.line_size);
+    summarize(&info);
 
-    println!("colour type {:?}", info.color_type);
 
-    let pixel_list = vec!(Pixel::default();info.buffer_size());
+    // let mut pixel_list = vec!(Pixel::default());
+    let mut pixel_list = Vec::with_capacity(info.buffer_size()/4);
+
+    println!("info buffer size {}", info.buffer_size()/4);
+    println!("Vec capacity {}", pixel_list.capacity());
+    println!("Vec len {}", pixel_list.len());
     
+
     // Grab the bytes of the image.
     let bytes = &buf[..info.buffer_size()];
-
     // Inspect more details of the last read frame.
-    const RGBA: u32 = 4;
-    bytes.iter().fold(0, |acc:u32, b| {
-        if (acc + 1) % 1 == 0 {
-            if (acc % info.width) == 0  && acc > 0 {
-                println!("{}", b);
-            }
-            else {
-                print!("{}", b);
-            }
-        } 
-        acc + 1
+    const RGBA: usize = 4;
+    bytes.iter().enumerate().fold(Pixel::default(), |mut acc: Pixel, (i,byte)| {
+        match (i + 1) % RGBA {
+            0 => {  acc.alpha = *byte;
+                    pixel_list.push(acc.clone())},
+            1 => acc.red = *byte,
+            2 => acc.green = *byte,
+            3 => acc.blue = *byte,
+            _ => panic!("inconcievable RGBA value {i}")
+        };
+        println!("vec len {} i{i}", pixel_list.len());
+
+        acc
     });
+
+    println!("Vec elements {}", pixel_list.capacity());
+    println!("Vec len {}", pixel_list.len());
+
+
     // let in_animation = reader.info().frame_control.is_some();
 }
 
@@ -48,4 +59,10 @@ impl Default for Pixel {
     fn default() -> Pixel {
         Pixel { red: 0, green: 0, blue: 0, alpha: 0 }
     }
+}
+
+fn summarize(info: &OutputInfo) {
+    println!("width {} * height {}", info.width, info.height);
+    println!("bit depth {:?}, line size {}", info.bit_depth, info.line_size);
+    println!("colour type {:?}", info.color_type);
 }
